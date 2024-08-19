beb4c3ffab693662891e8f1a93bff05f1dde4357
Alejandro Rosas Jaime <arosas-j@c3r10s2.42malaga.com>
2024-08-19 18:09:25 +0200

    prototipo de raycasting

--- arosas-j/raycasting/raycast.c
+++ arosas-j/raycasting/raycast.c
@@ -6,37 +6,81 @@
 /*   By: arosas-j <arosas-j@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 17:19:02 by arosas-j          #+#    #+#             */
-/*   Updated: 2024/08/17 22:25:33 by arosas-j         ###   ########.fr       */
+/*   Updated: 2024/08/19 17:44:30 by arosas-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycast.h"
+#include <limits.h>
 
-double	get_v_distance(t_player player, char **map, t_ray *ray)
+int	sign(double n)
+{
+	if (n > 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (0);
+}
+
+bool	wall_hit(double x, double y, char **map)
+{
+	x = floor(x / TILE_SIZE);
+	y = floor(y / TILE_SIZE);
+	if (map[(int)y][(int)x] == '1')
+		return (true);
+	return (false);
+}
+
+double	get_v_distance(t_player *player, char **map, t_ray *ray)
 {
 	double	step_x;
 	double	step_y;
 	double	x_pos;
 	double	y_pos;
 
-	step_x = TILE_SIZE;
-	step_y = 
-	
-	x_pos = 
-	y_pos =	TILE_SIZE;
+	step_x = TILE_SIZE * sign(cos(ray->angle));
+	step_y = TILE_SIZE * tan(ray->angle);
+	x_pos = floor(player->pos_x / TILE_SIZE) * TILE_SIZE;
+	if (cos(ray->angle) >= 0)
+		x_pos += TILE_SIZE;
+	y_pos =	player->pos_y + (x_pos - player->pos_x) * tan(ray->angle);
+	while (!wall_hit(x_pos, y_pos, map))
+	{
+		x_pos += step_x;
+		y_pos += step_y;
+	}
+	return (sqrt(pow(x_pos - player->pos_x, 2) + pow(y_pos - player->pos_y, 2)));
 }
 
-double	get_v_distance(t_player player, char **map, t_ray *ray)
-{}
+double	get_h_distance(t_player *player, char **map, t_ray *ray)
+{
+	double	step_x;
+	double	step_y;
+	double	x_pos;
+	double	y_pos;
 
-void	raycast(t_player player, char **map, t_ray *ray)
+	step_x = TILE_SIZE / tan(ray->angle);
+	step_y = TILE_SIZE * sign(sin(ray->angle));
+	y_pos = floor(player->pos_y / TILE_SIZE) * TILE_SIZE;
+	if (sin(ray->angle) >= 0)
+		y_pos +=  TILE_SIZE;
+	x_pos = player->pos_x + (y_pos - player->pos_y) / tan(ray->angle);
+	while (!wall_hit(x_pos, y_pos, map))
+	{
+		x_pos += step_x;
+		y_pos += step_y;
+	}
+	return (sqrt(pow(x_pos - player->pos_x, 2) + pow(y_pos - player->pos_y, 2)));
+}
+
+void	raycast(t_player *player, char **map, t_ray *ray)
 {
 	int	i;
 	double	v_distance;
 	double	h_distance;
 
 	i = 0;
-	//Calcular angulo del primer rayo
+	ray->angle = player->angle + player->fov / 2;
 	while (i < S_W)
 	{
 		v_distance = get_v_distance(player, map, ray);
@@ -45,8 +89,9 @@ void	raycast(t_player player, char **map, t_ray *ray)
 			ray->distance = v_distance;
 		else
 			ray->distance = h_distance;
-		//Render
+		ray->distance = ray->distance * cos(ray->angle);
+		render(ray);//to_do
 		i++;
-		ray->angle = new_angle();//Calcular el angulo del proximo rayo
+		ray->angle = ray->angle + player->fov / S_W;
 	}
 }
--- arosas-j/raycasting/raycast.h
+++ arosas-j/raycasting/raycast.h
@@ -6,26 +6,39 @@
 /*   By: arosas-j <arosas-j@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 17:18:42 by arosas-j          #+#    #+#             */
-/*   Updated: 2024/08/17 22:25:37 by arosas-j         ###   ########.fr       */
+/*   Updated: 2024/08/19 12:38:14 by arosas-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RAYCAST_H
 # define RAYCAST_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
 # include <math.h>
 # include "MLX42.h"
 
 # define S_W 1000
 # define TILE_SIZE 32
 
-typedef struct s_player
+typedef enum	e_side
+{
+	NORTH,
+	SOUTH,
+	WEST,
+	EAST
+}				t_side;
+
+typedef struct	s_player
 {
 	double	pos_x;
 	double	pos_y;
 	double	dir_x;
 	double	dir_y;
 	double	fov;
+	double	angle;
 }				t_player;
 
 typedef struct s_ray
@@ -36,6 +49,7 @@ typedef struct s_ray
 	double	h_x;
 	double	h_y;
 	double	distance;
+	t_side	side;
 }				t_ray;
 
 #endif
